8ea78705ba636c3c7ce33e3346b9316ff5165b55
Yoon Jeonghyun <chon_5@naver.com>
2019-12-04 20:37:46 +0900

    Create temporary data

---
+++ db.js
@@ -0,0 +1,61 @@
+export const videos = [{
+        id: 324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        vidoeFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Me",
+            email: "Me@me.com"
+        }
+    },
+    {
+        id: 123123,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        vidoeFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Me",
+            email: "Me@me.com"
+        }
+    },
+    {
+        id: 6235243,
+        title: 'Video hey',
+        description: 'This is something I love',
+        views: 24,
+        vidoeFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Me",
+            email: "Me@me.com"
+        }
+    },
+    {
+        id: 123537,
+        title: 'Video go',
+        description: 'This is something I love',
+        views: 24,
+        vidoeFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Me",
+            email: "Me@me.com"
+        }
+    },
+    {
+        id: 39263,
+        title: 'Video now',
+        description: 'This is something I love',
+        views: 24,
+        vidoeFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Me",
+            email: "Me@me.com"
+        }
+    }
+];
