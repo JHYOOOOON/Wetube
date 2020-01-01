9ed573012b8ee4798988fb7dc96748774e201a69
Yoon Jeonghyun <chon_5@naver.com>
2020-01-01 18:42:31 +0900

    Connect with mongodb

--- db.js
+++ db.js
@@ -1,61 +1,17 @@
-export const videos = [{
-        id: 324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Me",
-            email: "Me@me.com"
-        }
-    },
-    {
-        id: 123123,
-        title: 'Video super',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Me",
-            email: "Me@me.com"
-        }
-    },
-    {
-        id: 6235243,
-        title: 'Video hey',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Me",
-            email: "Me@me.com"
-        }
-    },
-    {
-        id: 123537,
-        title: 'Video go',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Me",
-            email: "Me@me.com"
-        }
-    },
-    {
-        id: 39263,
-        title: 'Video now',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Me",
-            email: "Me@me.com"
-        }
-    }
-];
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL, {
+        usesNewUrlParser: true,
+        useFindAndModify: false
+    });
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) => console.log(`Error on DB connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
