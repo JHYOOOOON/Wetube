85fb59ecb5118b1f6c5d250cb4a2013e65fc50a0
Yoon Jeonghyun <chon_5@naver.com>
2020-01-29 22:19:28 +0900

    Add routes about kakao, github login

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
+const ME = "/me";
 
 // Videos
 const VIDEOS = "/videos";
@@ -21,6 +22,10 @@ const DELETE_VIDEO = "/:id/delete";
 // Github
 const GITHUB = "/auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
+
+// Instagram
+const KAKAO = "/oauth/kakao";
+const KAKAO_CALLBACK = "/oauth/kakao/callback";
 
 const routes = {
     home: HOME,
@@ -62,7 +67,10 @@ const routes = {
         }
     },
     gitHub: GITHUB,
-    githubCallback: GITHUB_CALLBACK
+    githubCallback: GITHUB_CALLBACK,
+    me: ME,
+    kakao: KAKAO,
+    kakaoCallback: KAKAO_CALLBACK
 }
 
 export default routes;
