b4b80240d0fa3c2e81b624ba83f360730115dc8e
Yoon Jeonghyun <jhyooooooon@gmail.com>
2020-02-01 00:12:25 +0900

    Add comments, videos, creator

--- models/Video.js
+++ models/Video.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const VideoSchema = new mongoose.Schema({
     fileUrl: {
         type: String,
-        requird: "File URL is required"
+        required: "File URL is required"
     },
     title: {
         type: String,
@@ -21,8 +21,12 @@ const VideoSchema = new mongoose.Schema({
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment"
-    }]
-})
+    }],
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+});
 
 const model = mongoose.model("Video", VideoSchema);
 export default model;
