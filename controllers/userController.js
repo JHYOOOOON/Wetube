6103cef6c34382ae81b14c573e88863bd186bec9
Yoon Jeonghyun <jhyooooooon@gmail.com>
2020-02-01 00:28:45 +0900

    Edit get, post functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,6 @@ export const postJoin = async(req, res, next) => {
             await User.register(user, password);
             next();
         } catch (error) {
-            console.log(error);
             res.redirect(routes.home);
         }
     }
@@ -109,11 +108,51 @@ export const getMe = (req, res) => {
 export const userDetail = async(req, res) => {
     const { params: { id } } = req;
     try {
-        const user = await User.findById(id);
+        const user = await User.findById(id).populate("videos");
         res.render("userDetail", { pageTitle: "User Detail", user });
     } catch (error) {
         res.redirect(routes.home);
     }
 }
-export const editProfile = (req, res) => res.render('editProfile', { pageTitle: "Edit Profile" });
-export const changePassword = (req, res) => res.render('changePassword', { pageTitle: "Change Password" });
+export const getEditProfile = (req, res) => res.render('editProfile', { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async(req, res) => {
+    const {
+        body: { name, email },
+        file
+    } = req;
+    try {
+        await User.findByIdAndUpdate(req.user.id, {
+            name,
+            email,
+            avatarUrl: file ? file.path : req.user.avatarUrl
+        });
+        res.redirect(routes.me);
+    } catch (error) {
+        res.redirect(routes.editProfile);
+    }
+
+}
+
+export const getChangePassword = (req, res) => res.render('changePassword', { pageTitle: "Change Password" });
+
+export const postChangePassword = async(req, res) => {
+    const {
+        body: {
+            oldPassword,
+            newPassword,
+            newPassword1
+        }
+    } = req;
+    try {
+        if (newPassword !== newPassword1) {
+            res.status(400);
+            res.redirect(routes.changePassword);
+            return;
+        }
+        await req.user.changePassword(oldPassword, newPassword);
+        res.redirect(routes.me);
+    } catch (error) {
+        res.redirect(`/users${routes.changePassword}`);
+    }
+}
