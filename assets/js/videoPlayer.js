805c1f89492681846e1bac07bc297f94d2205e6c
Yoon Jeonghyun <jhyooooooon@gmail.com>
2020-02-10 23:39:39 +0900

    Edit method from GET to POST

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -10,7 +10,7 @@ const volumeRange = document.getElementById("jsVolume");
 const registerView = () => {
     const videoId = window.location.href.split("/videos/")[1];
     fetch(`/api/${videoId}/view`, {
-      method: "GET"
+      method: "POST"
     });
 };
 
