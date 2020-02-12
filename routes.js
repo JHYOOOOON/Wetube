8d5851dd381f1a22ce259a1be0c8974f60c63575
Yoon Jeonghyun <jhyooooooon@gmail.com>
2020-02-12 20:25:17 +0900

    Import and Install

--- routes.js
+++ routes.js
@@ -31,54 +31,56 @@ const KAKAO_CALLBACK = "/oauth/kakao/callback";
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
 const ADD_COMMENT = "/:id/comment";
+const DELETE_COMMENT = "/:id/commentdelete";
 
 const routes = {
-    home: HOME,
-    join: JOIN,
-    login: LOGIN,
-    logout: LOGOUT,
-    search: SEARCH,
-    users: USERS,
-    userDetail: (id) => {
-        if (id) {
-            return `/users/${id}`;
-        } else {
-            return USER_DETAIL;
-        }
-    },
-    editProfile: EDIT_PROFILE,
-    changePassword: CHANGE_PASSWORD,
-    videos: VIDEOS,
-    upload: UPLOAD,
-    videoDetail: (id) => {
-        if (id) {
-            return `/videos/${id}`;
-        } else {
-            return VIDEO_DETAIL;
-        }
-    },
-    editVideo: id => {
-        if (id) {
-            return `/videos/${id}/edit`;
-        } else {
-            return EDIT_VIDEO;
-        }
-    },
-    deleteVideo: (id) => {
-        if (id) {
-            return `/videos/${id}/delete`;
-        } else {
-            return DELETE_VIDEO;
-        }
-    },
-    gitHub: GITHUB,
-    githubCallback: GITHUB_CALLBACK,
-    me: ME,
-    kakao: KAKAO,
-    kakaoCallback: KAKAO_CALLBACK,
-    api: API,
-    registerView: REGISTER_VIEW,
-    addComment: ADD_COMMENT
-}
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: id => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: id => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: id => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: id => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  },
+  gitHub: GITHUB,
+  githubCallback: GITHUB_CALLBACK,
+  me: ME,
+  kakao: KAKAO,
+  kakaoCallback: KAKAO_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
+  deleteComment: DELETE_COMMENT
+};
 
 export default routes;
