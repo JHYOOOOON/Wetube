8c9d6aee83f39e868d777a21edf9b4b6e64f283c
Yoon Jeonghyun <jhyooooooon@gmail.com>
2020-02-12 20:24:40 +0900

    Add flash messages

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,156 +3,179 @@ import routes from "../routes";
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
-    res.render('join', { pageTitle: "Join" });
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async(req, res, next) => {
-    const {
-        body: { name, email, password, password2 }
-    } = req;
-    if (password !== password2) {
-        res.status(400);
-        res.render('join', { pageTitle: "Join" });
-    } else {
-        try {
-            const user = await User({
-                name,
-                email
-            });
-            await User.register(user, password);
-            next();
-        } catch (error) {
-            res.redirect(routes.home);
-        }
+export const postJoin = async (req, res, next) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    req.flash("error", "Passwords don't match");
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      res.redirect(routes.home);
     }
+  }
 };
 
-export const getLogin = (req, res) => res.render('login', { pageTitle: "Login" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 export const postLogin = passport.authenticate("local", {
-    failureRedirect: routes.login,
-    successRedirect: routes.home
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+  successFlash: "Welcome",
+  failureFlash: "Can't log in. Check email and/or password"
 });
 
-export const githubLogin = passport.authenticate("github");
+export const githubLogin = passport.authenticate("github", {
+  successFlash: "Welcome",
+  failureFlash: "Can't log in. Check email and/or password"
+});
 
-export const githubLoginCallback = async(accessToken, refreshToken, profile, cb) => {
-    const {
-        _json: { id, avatar_url, name, email }
-    } = profile;
-    try {
-        const user = await User.findOne({ email });
-        if (user) {
-            user.githubId = id;
-            user.save();
-            return cb(null, user);
-        }
-        const newUser = await User.create({
-            email,
-            name,
-            githubId: id,
-            avatarUrl: avatar_url
-        });
-        return cb(null, newUser);
-
-    } catch (error) {
-        return cb(error);
+export const githubLoginCallback = async (
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) => {
+  const {
+    _json: { id, avatar_url, name, email }
+  } = profile;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      user.githubId = id;
+      user.save();
+      return cb(null, user);
     }
+    const newUser = await User.create({
+      email,
+      name,
+      githubId: id,
+      avatarUrl: avatar_url
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
+  }
 };
 
 export const postGithubLogIn = (req, res) => {
-    res.redirect(routes.home);
-}
+  res.redirect(routes.home);
+};
 
-export const kakaoLogin = passport.authenticate("kakao");
+export const kakaoLogin = passport.authenticate("kakao", {
+  successFlash: "Welcome",
+  failureFlash: "Can't log in. Check email and/or password"
+});
 
-export const kakaoLoginCallback = async(accessToken, refreshToken, profile, cb) => {
-    const {
-        _json: {
-            id,
-            properties: { profile_image: avatar_url, nickname: name },
-            kakao_account: { email }
-        }
-    } = profile;
-    try {
-        const user = await User.findOne({ email });
-        if (user) {
-            user.kakaoId = id;
-            user.save();
-            return cb(null, user);
-        }
-        const newUser = await User.create({
-            email,
-            name,
-            githubId: id,
-            avatarUrl: avatar_url
-        });
-        return cb(null, newUser);
-
-    } catch (error) {
-        return cb(error);
+export const kakaoLoginCallback = async (
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) => {
+  const {
+    _json: {
+      id,
+      properties: { profile_image: avatar_url, nickname: name },
+      kakao_account: { email }
     }
+  } = profile;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      user.kakaoId = id;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      email,
+      name,
+      githubId: id,
+      avatarUrl: avatar_url
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
+  }
 };
 
 export const postKakaoLogIn = (req, res) => {
-    res.redirect(routes.home);
-}
+  res.redirect(routes.home);
+};
 
 export const logout = (req, res) => {
-    req.logout();
-    res.redirect(routes.home);
+  req.flash("info", "Logged out, see you later!");
+  req.logout();
+  res.redirect(routes.home);
 };
 
 export const getMe = (req, res) => {
-    res.render('userDetail', { pageTitle: "User Detail", user: req.user })
+  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
 };
-export const userDetail = async(req, res) => {
-    const { params: { id } } = req;
-    try {
-        const user = await User.findById(id).populate("videos");
-        res.render("userDetail", { pageTitle: "User Detail", user });
-    } catch (error) {
-        res.redirect(routes.home);
+export const userDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const user = await User.findById(id).populate("videos");
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    req.flash("error", "User not found");
+    res.redirect(routes.home);
+  }
+};
+export const getEditProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file
+  } = req;
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl
+    });
+    req.flash("success", "Profile updated");
+    res.redirect(routes.me);
+  } catch (error) {
+    req.flash("error", "Can't update profile");
+    res.redirect(routes.editProfile);
+  }
+};
+
+export const getChangePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
+
+export const postChangePassword = async (req, res) => {
+  const {
+    body: { oldPassword, newPassword, newPassword1 }
+  } = req;
+  try {
+    if (newPassword !== newPassword1) {
+      req.flash("error", "Password don't match");
+      res.status(400);
+      res.redirect(routes.changePassword);
+      return;
     }
-}
-export const getEditProfile = (req, res) => res.render('editProfile', { pageTitle: "Edit Profile" });
-
-export const postEditProfile = async(req, res) => {
-    const {
-        body: { name, email },
-        file
-    } = req;
-    try {
-        await User.findByIdAndUpdate(req.user.id, {
-            name,
-            email,
-            avatarUrl: file ? file.path : req.user.avatarUrl
-        });
-        res.redirect(routes.me);
-    } catch (error) {
-        res.redirect(routes.editProfile);
-    }
-
-}
-
-export const getChangePassword = (req, res) => res.render('changePassword', { pageTitle: "Change Password" });
-
-export const postChangePassword = async(req, res) => {
-    const {
-        body: {
-            oldPassword,
-            newPassword,
-            newPassword1
-        }
-    } = req;
-    try {
-        if (newPassword !== newPassword1) {
-            res.status(400);
-            res.redirect(routes.changePassword);
-            return;
-        }
-        await req.user.changePassword(oldPassword, newPassword);
-        res.redirect(routes.me);
-    } catch (error) {
-        res.redirect(`/users${routes.changePassword}`);
-    }
-}
+    await req.user.changePassword(oldPassword, newPassword);
+    res.redirect(routes.me);
+  } catch (error) {
+    req.flash("error", "Can't change password");
+    res.redirect(`/users${routes.changePassword}`);
+  }
+};
